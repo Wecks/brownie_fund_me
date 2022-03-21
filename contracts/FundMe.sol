f955ab9cb884d7c5693a8e43b1564363fab5bf5d
Wecks <alexislemaire37@gmail.com>
2022-03-21 15:49:10 +0100

    first commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
+contract FundMe {
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address _priceFeed) public {
+        // executed directly when we deploy our contract
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        // 5 $
+        uint256 minimumUSD = 5 * 10**18;
+        // 1Gwei < 5$
+        require(
+            getConversionRate(msg.value) >= minimumUSD,
+            "You need to spend more ETH!"
+        );
+        addressToAmountFunded[msg.sender] += msg.value;
+        //what the ETH -> USD conversion rate -> How to get this information : Oracle : Chainlink
+        funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns (uint256) {
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns (uint256) {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return uint256(answer * 10**10); // obtenir le prix Wei
+        // 2698.29163009
+    }
+
+    //10000000000
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 10**18;
+        return ethAmountInUsd;
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        //minimum USD
+        uint256 minimumUSD = 5 * 10**18;
+        uint256 price = getPrice();
+        uint256 precision = 1 * 10**18;
+        return ((minimumUSD * precision) / price) + 1;
+    }
+
+    //modifier: https://medium.com/coinmonks/solidity-tutorial-all-about-modifiers-a86cf81c14cb
+    modifier onlyOwner() {
+        //is the message sender owner of the contract?
+        require(msg.sender == owner);
+
+        _;
+    }
+
+    // onlyOwner modifer will first check the condition inside it
+    // and
+    // if true, withdraw function will be executed
+    function withdraw() public payable onlyOwner {
+        // If you are using version eight (v0.8) of chainlink aggregator interface,
+        // you will need to change the code below to
+        // payable(msg.sender).transfer(address(this).balance);
+        msg.sender.transfer(address(this).balance);
+
+        //iterate through all the mappings and make them 0
+        //since all the deposited amount has been withdrawn
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        //funders array will be initialized to 0
+        funders = new address[](0);
+    }
+}
